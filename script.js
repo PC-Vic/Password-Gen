6bafc12578d93ecf909943a0a3e1c3d98aff9a15
Victor Nieves <c_cc_vic@yahoo.com>
2023-06-22 18:05:20 -0400

    fixed undefined problem, generator working properly now

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
 // Assignment code here
-var charMin = 8
-var charMax = 128
-var upperCaseLetters = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
-var charNumbers = ['0123456789']
-var specialChar = ['!@#$%&*+=/?()']
+var charMin = 8;
+var charMax = 128;
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var charNumbers = "0123456789";
+var specialChar = "!@#$%&*+=/?()";
 
-var finalArr = [upperCaseLetters,charNumbers, specialChar]
+function generatePassword() {
+  var passwordLength = parseInt(
+    prompt("Please specify the length of the password you want to create")
+  );
+  var finalArray = "";
 
-function generatePassword () {
-  var passwordLength = prompt("Please specify the length of the password you want to create")
-  console.log(passwordLength)
-  if (passwordLength < 8 ||  passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("characters length not acceptable");
-  }
-  var upperCase = confirm("would you like to include uppercase characters?")
+  } else {
+    var includeCaseLetters = confirm(
+      "would you like to include upper-case characters?"
+    );
+    if (includeCaseLetters === true) {
+      finalArray += upperCaseLetters;
+    }
+    // console.log(finalArray);
+    var charSpecial = confirm("would you like to add any special characters?");
+    if (charSpecial === true) {
+      finalArray += specialChar;
+    }
+    var lowerLetters = confirm(
+      "would you like to use any lower-case characters?"
+    );
+    if (lowerLetters === true) {
+      finalArray += lowerCaseLetters;
+    }
+    var numberRandom = confirm("would you like to use any numbers?");
+    if (numberRandom === true) {
+      finalArray += charNumbers;
+    }
+    finalArray = finalArray.split("");
+    var passWordArray = "";
+    // console.log(finalArray);
+    for (var i = 0; i < passwordLength; i++)
+    {
+      var randomIndex = Math.floor(Math.random() * finalArray.length);
+      passWordArray += finalArray[randomIndex];
+     
+    }
+   
 
-  if (upperCaseLetters ===true){
-    console.log(finalArr)
     //add upper case letters to my final Arr
+    return passWordArray
   }
-
-
-
-
-
-
-  // finalArr will have all the character they will need and from there you will need a for loop to get random characters
-  return(upperCaseLetters, charNumbers,specialChar)
 }
+
+// finalArr will have all the character they will need and from there you will need a for loop to get random characters
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -38,7 +63,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
